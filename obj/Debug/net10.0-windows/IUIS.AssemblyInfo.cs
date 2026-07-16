bdf9a36bfa02df3e1ed9445f59889c1aa8663139
AsherDoesTechs <Asherbasco93@gmail.com>
2026-07-16 21:50:28 +0800

    Added Changes

--- obj/Debug/net10.0-windows/IUIS.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/IUIS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IUIS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6fffe899470c1721562bf6826d3d90dc5d3a257")]
 [assembly: System.Reflection.AssemblyProductAttribute("IUIS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IUIS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
